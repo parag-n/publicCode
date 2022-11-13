253ea1408904513475597b4806bbc2630c5b40aa
Parag Nukalwar <112768770+parag-n@users.noreply.github.com>
2022-11-13 13:23:02 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,14 +8,16 @@ var onDefault=function(req, res){
               "<ol>"+
 	    "<head><b>Follow these commands with \"SHIDDAT\" to run the code successfully!</b>"+
                     " <li><br/>sudo apt update</li>"+
-                    " <li>git clone https://github.com/RaviTambade/dbdaMarch22</li>"+
-                    " <li>cd dbdaMarch22</li>"+
+                    " <li>git clone https://github.com/parag-n/publicCode</li>"+
+                    " <li>cd publicCode</li>"+
+	     	    " <li>rm -r webapp</li>"+
                     " <li>sudo apt install -y docker.io</li>"+
-                    " <li>sudo docker build -t ravya .</li>"+
-                    " <li>sudo docker run --name server -d -p 9000:9000 ravya:latest</li>"+
+                    " <li>sudo docker build -t <b style="color:Tomato;"image_name</b> .</li>"+
+                    " <li>sudo docker run --name <b style="color:Tomato;"server_name</b> -d -p 9000:9000 <b style="color:Tomato;"image_name</b>:latest</li>"+
                     " <li>check and open the ip address</li>"+
                     " <li>don't forget to check the security settings</li>"+
                     " <b>hasta la vista</b>"+
+	     "</head>"+
              "</ol>");
 };
 
