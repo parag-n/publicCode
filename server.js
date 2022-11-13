9e25ee63d8c59814871bd6577733a15cc2c42319
parag-n <paragother@gmail.com>
2022-11-13 20:03:46 +0530

    updated the code

--- server.js
+++ server.js
@@ -6,18 +6,19 @@ var onDefault=function(req, res){
     res.send("<h1><center>Hello Bitches! This is your dad</center></h1>"+
               "<hr/>"+
               "<ol>"+
-	    "<head><b>Follow these commands with \"SHIDDAT\" to run the code successfully!</b>"+
-                    " <li><br/>sudo apt update</li>"+
+	    "<head background = \"Golden_Retriever.jpg\" ><b>Follow these commands with \"SHIDDAT\" to run the code successfully!</b>"+
+                    " <li>sudo apt update</li>"+
                     " <li>git clone https://github.com/parag-n/publicCode</li>"+
                     " <li>cd publicCode</li>"+
                     " <li>sudo apt install -y docker.io</li>"+
-                    " <li>sudo docker build -t <b style="color:Tomato;"image_name</b> .</li>"+
-                    " <li>sudo docker run --name <b style="color:Tomato;"server_name</b> -d -p 9000:9000 <b style="color:Tomato;"image_name</b>:latest</li>"+
+                    " <li>sudo docker build -t <b style=\"color:Tomato;\">image_name</b> .</li>"+
+                    " <li>sudo docker run --name <b style=\"color:Tomato;\">server_name</b> -d -p 9000:9000 <b style=\"color:Tomato;\">image_name</b>:latest</li>"+
                     " <li>check and open the ip address</li>"+
                     " <li>don't forget to check the security settings</li>"+
                     " <b>hasta la vista</b>"+
 	     "</head>"+
-             "</ol>");
+             "</ol>"+
+			 "<body>Note: names in color can be your custom name</body>");
 };
 
 var onAboutUs=function(req, res){
